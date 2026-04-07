20bb4223a80ad5564ea3affce20132361d30ba58
unknown <Dmytro Herman>
2026-04-07 11:10:28 +0300

    Default values have been added to the initializers

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True) \
-            -> None:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -17,8 +16,8 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, 3, is_hungry)
+    def __init__(self, name: str, appetite: int = 3, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite, is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
@@ -26,8 +25,8 @@ class Cat(Animal):
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, 7, is_hungry)
+    def __init__(self, name: str, appetite: int = 7, is_hungry: bool = True) -> None:
+        super().__init__(name, appetite, is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
